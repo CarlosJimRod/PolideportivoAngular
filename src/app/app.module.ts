fc3ddd95cfbe1497785a6b65ef39c2e90ddcd010
Cjr37 <carlosclase37@gmail.com>
2024-02-01 16:04:49 +0100

    correciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {ComunModule} from "./comun/comun.module";
+import {PartidosModule} from "./partidos/partidos.module";
+import {EquiposModule} from "./equipos/equipos.module";
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import {ComunModule} from "./comun/comun.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComunModule
+    ComunModule,
+    PartidosModule,
+    EquiposModule
   ],
   providers: [],
   bootstrap: [AppComponent]
