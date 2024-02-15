9221acafd620e4182e84f97d15bb0abaab56f0d9
Cjr37 <carlosclase37@gmail.com>
2024-02-15 02:30:36 +0100

    avances formulario y detalle

--- src/app/partidos/partido-form/partido-form.component.ts
+++ src/app/partidos/partido-form/partido-form.component.ts
@@ -1,29 +1,47 @@
-import {Component} from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 import {ValidacionForm} from "./ValidacionForm";
+import {PartidosService} from "../../servicios/partidos.service";
+import {Partido} from "../../models/partidos";
 
 @Component({
   selector: 'app-partido-form',
   templateUrl: './partido-form.component.html',
   styleUrls: ['./partido-form.component.css']
 })
-export class PartidoFormComponent {
-  resultado!: string
+export class PartidoFormComponent implements OnInit {
+  partidos: Partido[]
+  idPartido: number
   equipos = ["--selecciona un equipo--", "ATHCLU(ESP)", "DEPORT(ESP)", "FCBARC(ESP)", "VIREAL(ESP)", "MALAGA(ESP)", "EIBAR(ESP)", "GETAFE(ESP)", "BETIS(ESP)", "RMADRID(ESP)", "LEVANT(ESP)"]
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private partidosService: PartidosService) {
   }
 
   formPartido = this.fb.group({
     equipoLocal: ["--selecciona un equipo--", ValidacionForm.equipoRequired],
-    equipoVisitante: ["--selecciona un equipo--", ValidacionForm.equipoRequired]
+    equipoVisitante: ["--selecciona un equipo--", ValidacionForm.equipoRequired],
+    date: ['', Validators.required],
+    time: ['', Validators.required]
   })
+
+  ngOnInit() {
+    this.partidosService.getPartidos().subscribe(partidos =>
+      this.partidos = partidos
+    )
+    this.idPartido = this.partidos.length
+  }
 
   submit() {
     if (this.formPartido.valid) {
-      this.resultado = "Se han guardado los valores"
-    } else {
-      this.resultado = "Hay datos invalidos en el formulario"
+      this.partidosService.setPartido({
+        idPartido: this.idPartido.toString(),
+        idLocal: this.formPartido.value.equipoLocal!,
+        idVisitante: this.formPartido.value.equipoVisitante!,
+        fechaHora: this.formPartido.value.date!.toString() + " " + this.formPartido.value.time!.toString(),
+        goles: [],
+        tarjetas: []
+      })
+      this.formPartido.reset()
     }
   }
 }
--- src/app/servicios/partidos.service.ts
+++ src/app/servicios/partidos.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {Partido} from "../models/partidos";
 import {partidos_data} from "../data/partidos.data";
@@ -10,5 +10,9 @@ export class PartidosService {
 
   getPartidos () : Observable<Partido[]> {
     return of(partidos_data)
+  }
+
+  setPartido(partido: Partido) {
+    partidos_data.push(partido)
   }
 }
